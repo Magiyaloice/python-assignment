5a63c3f12d64853438031cfe273b8f9d2e5e8195
loice dube <loicekadube@gmail.com>
2024-09-08 18:27:28 +0200

    added week 2 assignment

--- user_input.py
+++ user_input.py
@@ -1,5 +1,22 @@
-name = input('Enter your name:')
-age = input("Enter your age: ")
-location = input( "Enter your location: ")
+# name = input('Enter your name:')
+# age = input("Enter your age: ")
+# location = input( "Enter your location: ")
 
-print(f"Hello {name}, you are {age} years old and live in{location}.")
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(2,15)
+
+list2 = [50,60,70]
+
+my_list.extend(list2)
+
+my_list.pop()
+my_list.sort()
+
+index = my_list.index(30)
+print (f"the index of {30} is: {index}") 
+
+print(my_list)
